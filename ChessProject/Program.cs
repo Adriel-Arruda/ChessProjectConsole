235d67f2ba74d221edf30bea9714af2bcfc5b6f0
Adriel Arruda <adriel.marcos.arruda@gmail.com>
2022-01-04 14:36:24 -0300

    Improving BoardPrint method implementation

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -1,16 +1,30 @@
 ﻿using BoardChess;
 using Chess;
-
 namespace ChessProject
 {
     public static class Program
     {
-        public static void Main(string[] args) 
+        public static void Main(string[] args)
         {
-            ChessPosition chessPosition = new ChessPosition('a', 1);
-            Console.WriteLine(chessPosition);
-            Console.WriteLine(chessPosition.ToPosition());
-        
+            try
+            {
+                Board board = new Board(8, 8);
+
+                board.PutPiece(new Tower(board, Color.Preta), new Position(0, 0));
+                board.PutPiece(new Tower(board, Color.Preta), new Position(1, 3));
+                board.PutPiece(new King(board, Color.Preta), new Position(2, 4));
+
+                board.PutPiece(new Tower(board, Color.Branca), new Position(7, 7));
+                board.PutPiece(new Tower(board, Color.Branca), new Position(5, 1));
+                board.PutPiece(new King(board, Color.Branca), new Position(2, 6));
+
+                Screen.BoardPrint(board);
+                Console.ReadLine();
+            }
+            catch (BoardException error)
+            {
+                Console.WriteLine(error.Message);
+            }
             Console.ReadLine();
         }
     }
--- ChessProject/Screen.cs
+++ ChessProject/Screen.cs
@@ -8,6 +8,7 @@ namespace ChessProject
         {
             for (int i = 0; i < board.Rows; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.Colunms; j++)
                 {
                     if (board.Piece(i, j) == null)
@@ -16,10 +17,26 @@ namespace ChessProject
                     }
                     else
                     {
-                        Console.Write(board.Piece(i, j) + " ");
+                        Screen.PrintPiece(board.Piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void PrintPiece(Piece piece)
+        {
+            if (piece.Color == Color.Branca)
+            {
+                Console.Write(piece);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
             }
         }
     }
