9f3ae2c1a2b14ba50b40ed0424be41aaccb9ff65
Adriel Arruda <adriel.marcos.arruda@gmail.com>
2022-01-04 11:51:28 -0300

    Creating the Position class

---
+++ ChessProject/Board/Position.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Board
+{
+    internal class Position
+    {
+        public int Row { get; set; }
+        public int Colunm { get; set; }
+
+        public Position(int row, int colunm)
+        {
+            Row = row;
+            Colunm = colunm;
+        }
+
+        public override string ToString()
+        {
+            return Row + ", " + Colunm;
+        }
+
+
+    }
+}
--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using Board;
 namespace ChessProject
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
+            Position position = new Position(3, 4);
+            Console.WriteLine(position);
 
         }
     }
